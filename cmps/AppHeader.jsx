3c58e011bd304479810522b61f911ce87a61b783
klilahkenazi <klilas2483@gmail.com>
2023-09-19 19:21:19 +0300

    F/add-app-header/ added name to the header

--- cmps/AppHeader.jsx
+++ cmps/AppHeader.jsx
@@ -8,12 +8,12 @@ export function AppHeader() {
 
     return (
         <header className="app-header">
+            <h1>Contacts App</h1>
             <nav>
                 <NavLink to="/">Home</NavLink> |
                 <NavLink to="/contact">Contacts</NavLink> |
                 <NavLink to="/about">About</NavLink> |
             </nav>
-            <h1>Contacts App</h1>
         </header>
     );
 }
