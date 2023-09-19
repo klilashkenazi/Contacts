aaa45bea5a704bb192e8044b112a171ef92c0176
klilahkenazi <klilas2483@gmail.com>
2023-09-19 18:09:25 +0300

    F/add-contact-edit/ created edit routes

--- RootCmp.jsx
+++ RootCmp.jsx
@@ -9,6 +9,7 @@ import { HomePage } from './pages/HomePage.jsx'
 import { AboutUs } from './pages/AboutUs.jsx'
 import { ContactIndex } from './pages/ContactIndex.jsx'
 import { store } from './store/store.js'
+import { ContactEdit } from './pages/ContactEdit.jsx'
 // import { CarDetails } from './pages/CarDetails.jsx'
 
 
@@ -25,6 +26,8 @@ export function App() {
                             <Route element={<AboutUs />} path="/about" />
                             <Route element={<ContactIndex />} path="/contact" />
                             {/* <Route element={<CarDetails />} path="/car/:carId" /> */}
+                            <Route element={<ContactEdit/>} path='/contact/Edit'/>
+                            <Route element={<ContactEdit/>} path='/contact/Edit/:contactId'/>
                         </Routes>
                     </main>
                     {/* <AppFooter /> */}
